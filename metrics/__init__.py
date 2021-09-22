8c84274f7eb3c3e4acbb4c28d96556d6e2e27dd2
ronfechtner <ronfechtner@gmail.com>
2021-09-22 13:45:31 +0200

    added opts by dict

---
+++ metrics/__init__.py
@@ -0,0 +1,100 @@
+import numpy as np
+
+from scipy import ndimage as ndi
+
+from sklearn.cluster import DBSCAN
+from skimage.feature import peak_local_max
+from skimage.segmentation import watershed
+import skimage.measure as skmeas
+
+
+def find_peaks(img, threshold_rel=0.05, min_distance=1, eps=7.5, min_samples=1, max_val=255):
+
+    peaks = peak_local_max(
+      img, threshold_rel=threshold_rel,
+      min_distance=min_distance, exclude_border=False
+    )
+
+    if len(peaks) > 0:
+        clustering = DBSCAN(eps=eps, min_samples=min_samples).fit(peaks)
+        peaks_clustered = np.array(
+          [
+            np.mean(peaks[clustering.labels_ == i], axis=0)
+            for i in range(np.max(clustering.labels_) + 1)
+          ]
+        )
+
+        # make an image with peaks at 1
+        peak_im = np.zeros_like(img)
+        for p in peaks_clustered.astype(int):
+            peak_im[p[0], p[1]] = 1
+
+        # label peaks
+        peak_label, n_labels = ndi.label(peak_im)
+
+        # propagate peak labels with watershed
+        labels = watershed(max_val - img, peak_label)
+
+        # limit watershed labels to area where the image is intense enough
+        labels = labels * (img > threshold_rel * max_val)
+        centers = np.array([[dim.mean() for dim in np.where(labels == l)] for l in np.unique(labels) if l != 0])
+
+        return labels, centers
+    else:
+        return np.array([]), np.array([])
+
+
+def peak_based_f1(true, pred, thT = 0.05, thP = 0.05, eps=8):
+    labels_true, centers_true = find_peaks(
+        true, threshold_rel=thT, eps=eps
+    )
+
+    labels_pred, centers_pred = find_peaks(
+        pred, threshold_rel=thP, eps=eps
+    )
+
+    TP = 0
+    FP = 0
+    FN = 0
+
+    TP_centers = []
+    FP_centers = []
+    FN_centers = []
+
+    if centers_true.shape[0] == 0:
+        FP = centers_pred.shape[0]
+        FP_centers = centers_pred
+    elif centers_pred.shape[0] == 0:
+        FN = centers_true.shape[0]
+        FN_centers = centers_true
+    else:
+        matches = match_descriptors(
+            centers_true, centers_pred,
+            metric='minkowski', p=2,
+            max_distance=eps,
+            cross_check=True, max_ratio=1.0
+        )
+
+        TP = matches.shape[0]
+        FP = centers_pred.shape[0] - TP
+        FN = centers_true.shape[0] - TP
+
+        TP_centers = np.array([[list(centers_true[m[0]]), list(centers_pred[m[1]])] for m in matches])
+        FP_centers = np.array([list(cp) for p, cp in enumerate(centers_pred) if p not in matches[:, 1]])
+        FN_centers = np.array([list(ct) for p, ct in enumerate(centers_true) if p not in matches[:, 0]])
+
+    precision = np.divide(TP, TP + FP) if TP + FP > 0 else 1.
+    recall = np.divide(TP, TP + FN) if TP + FN > 0 else 1.
+    f1 = np.divide(2 * precision * recall, precision + recall) if precision + recall > 0 else 0.
+
+    return {
+        "TP": TP,
+        "FP": FP,
+        "FN": FN,
+        "precision": precision,
+        "recall": recall,
+        "f1": f1,
+        "TP_centers": TP_centers,
+        "FP_centers": FP_centers,
+        "FN_centers": FN_centers
+    }
--- tune.py
+++ tune.py
@@ -25,9 +25,22 @@ from models import create_model
 from util.visualizer import Visualizer
 from sklearn.utils import Bunch
 import shlex
+from ray import tune
+from metrics import peak_based_f1
 
 
-def tune(opt):
+def tune(config):
+
+    ### Get training options from config
+    opt_str = """\
+        --model pix2pix \
+        --dataroot "{dataroot}" \
+        --name test \
+        --input_nc 1 \
+        --output_nc 1 \
+        --display_id 0""".format(**config)
+    opt = TrainOptions().parse(opt_str=opt_str)
+
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     dataset_size = len(dataset)    # get the number of images in the dataset.
     print('The number of training images = %d' % dataset_size)
@@ -38,14 +51,12 @@ def tune(opt):
 
     model = create_model(opt)      # create a model given opt.model and other options
     model.setup(opt)               # regular setup: load and print networks; create schedulers
-    visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots
     total_iters = 0                # the total number of training iterations
 
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
         epoch_start_time = time.time()  # timer for entire epoch
         iter_data_time = time.time()    # timer for data loading per iteration
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
-        visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
         model.update_learning_rate()    # update learning rates in the beginning of every epoch.
         for i, data in enumerate(dataset):  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
@@ -60,7 +71,6 @@ def tune(opt):
             if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
                 save_result = total_iters % opt.update_html_freq == 0
                 model.compute_visuals()
-                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
@@ -75,17 +85,37 @@ def tune(opt):
                 model.save_networks(save_suffix)
 
             iter_data_time = time.time()
+
         if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
             print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
             model.save_networks('latest')
             model.save_networks(epoch)
 
+        if epoch % config['val_freq'] == 0:
+            print('running validation at the end of epoch %d' % (epoch))
+            for i, data in enumerate(val_dataset):
+                if i >= config['max_val']:  # only apply our model to opt.num_test images.
+                    break
+                model.set_input(data)  # unpack data from data loader
+                model.test()  # run inference
+                visuals = model.get_current_visuals()  # get image results
+                #img_path = model.get_image_paths()  # get image paths
+
+                ### Calculate validation score
+                f1 = peak_based_f1(visuals['real_B'], visuals['fake_B'])
+
+                tune.report(f1=f1)
+
         print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
 
 
 if __name__ == '__main__':
-    opt_str = '--model pix2pix --dataroot "./../../Datasets/new/BF2FLAVG_cropped_4th_rnd3rd/" --name test --input_nc 1 --output_nc 1 --display_id 0'
-    opt = TrainOptions().parse(opt_str=opt_str)
 
-    print(opt)
-    tune(opt)
+    analysis = tune.run(
+        tune,
+        config={
+            "max_dataset_size": 10,
+            "dataroot": "./../../Datasets/new/BF2FLAVG_cropped_4th_rnd3rd/",
+            "val_feq": 1,
+            "max_val": 5
+        })
