2d9d59572d6af5f64173d1ddb745628c82bede97
ronfechtner <ronfechtner@gmail.com>
2021-09-22 14:25:24 +0200

    minor typo

--- train.py
+++ train.py
@@ -23,6 +23,7 @@ from options.train_options import TrainOptions
 from data import create_dataset
 from models import create_model
 from util.visualizer import Visualizer
+from util import util
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
